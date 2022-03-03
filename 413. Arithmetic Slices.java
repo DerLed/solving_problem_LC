13e1b776486412db0c459e44731df129ddd4cd3e
DerLed <48630545+DerLed@users.noreply.github.com>
2022-03-03 16:45:29 +0300

    Update 413. Arithmetic Slices.java

--- 413. Arithmetic Slices.java
+++ 413. Arithmetic Slices.java
@@ -19,9 +19,21 @@
                 result++;
                 full = false;
             }
+            else  full = false;
             System.out.printf("%d %d\n", qw1, qw2);
             start++;
         }
         if (full && nums.length > 4) result++;
         return result;
     }
+
+/*TEST
+[1,2,3,4]
+[1,2,3,4, 5]
+[7,7,7,7]
+[1,3,5,7,9]
+[3,-1,-5,-9]
+[1,2,3,4,8]
+[7,2,3,4,5]
+[1,2,3,10,4,5,6]
+*/
